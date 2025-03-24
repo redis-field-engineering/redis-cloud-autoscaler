f71164b569af4f75577f2e11f63ba65c4af807c3
slorello89 <steve.lorello@redis.com>
2025-03-24 15:34:43 -0400

    adding validation exception handler

--- autoscaler/redis-cloud-autoscaler/src/main/java/com/redis/autoscaler/ValidationExceptionHandler.java
+++ autoscaler/redis-cloud-autoscaler/src/main/java/com/redis/autoscaler/ValidationExceptionHandler.java
@@ -1,6 +1,5 @@
 package com.redis.autoscaler;
 
-import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
