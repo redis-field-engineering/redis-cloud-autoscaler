041a01478ea40a3ccb09f905b49e0210b24ad748
slorello89 <steve.lorello@redis.com>
2025-09-17 10:32:53 -0400

    adding tls support

--- autoscaler/redis-cloud-autoscaler/src/main/java/com/redis/autoscaler/RedisConfig.java
+++ autoscaler/redis-cloud-autoscaler/src/main/java/com/redis/autoscaler/RedisConfig.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisClientConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 
@@ -18,9 +19,9 @@ public class RedisConfig {
 		int redisPort = Integer.parseInt(redisHostAndPort.split(":")[1]);
 		String redisPassword = System.getenv("REDIS_PASSWORD");
 		String redisUser = System.getenv("REDIS_USER") != null ? System.getenv("REDIS_USER") : "default";
+        boolean useSsl = System.getenv("REDIS_USE_SSL") != null && System.getenv("REDIS_USE_SSL").equalsIgnoreCase("true");
         RedisStandaloneConfiguration config = new RedisStandaloneConfiguration(redisHost, redisPort);
         LOG.info("connecting to redis at {}:{}", redisHost, redisPort);
-
         if(redisPassword != null && !redisPassword.isEmpty()) {
             config.setPassword(redisPassword);
         }
@@ -28,8 +29,12 @@ public class RedisConfig {
         if (redisUser != null && !redisUser.isEmpty()) {
             config.setUsername(redisUser);
         }
-
-        return new JedisConnectionFactory(config);
+        JedisClientConfiguration.JedisClientConfigurationBuilder jedisClientConfigurationBuilder = JedisClientConfiguration.builder();
+        if(useSsl) {
+            LOG.info("Using SSL to connect to Redis");
+            jedisClientConfigurationBuilder.useSsl();
+        }
+        return new JedisConnectionFactory(config, jedisClientConfigurationBuilder.build());
     }
 
     @Bean
